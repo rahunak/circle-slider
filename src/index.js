ef7118a0b8123020cd75e68ecd10d979071541ce
Zaiko Eugene <euhene@linnovate.net>
2023-01-17 18:29:21 +0300

    feat: init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// css
+import "./components/css/style.css";
+// scss
+import "./components/scss/main.scss";
+import "./components/scss/fonts.scss";
+
+// html
+// import './index.html';// вообще надо?
+import "./components/html/secondPage.html";
+// scripts
+import "./components/scripts/main";
