67feb9365ab61c8e53f0b2119a9f4f20e11b4f91
Zaiko Eugene <euhene@linnovate.net>
2023-01-18 14:26:59 +0300

    try to understand how correctly deploy on gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
 
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "slider"),
     filename: "[name].bundle.js",
   },
   module: {
